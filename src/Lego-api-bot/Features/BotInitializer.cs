1d0e35a30661c35c60109b699744d57bbb2bac5b
Всеволод Паршин <seva.parshin@yandex.ru>
2020-05-02 00:03:33 +0300

    Added bot project

---
+++ src/Lego-api-bot/Features/BotInitializer.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace Lego_api_bot.Features
+{
+    public class BotInitializer
+    {
+        static TelegramBotClient _botClient;
+        static ILogger _logger;
+
+        static BotInitializer()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true)
+                .Build();
+
+            _logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            HttpClient client = new HttpClient(clientHandler);
+
+            var token = config["BotToken"];
+            _botClient = new TelegramBotClient(token, client);
+        }
+
+        public static async Task StartWork()
+        {
+            var me = await _botClient.GetMeAsync();
+            _logger.Information($"My id: {me.Id} my name: {me.FirstName} {me.LastName}");
+            
+            _botClient.StartReceiving();
+            _botClient.OnMessage += ProcessMessage;
+        }
+
+        private static async void ProcessMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            var message = e.Message;
+            await _botClient.SendTextMessageAsync(message.Chat, "Hello world");
+        }
+    }
+}
---
+++ src/Lego-api-bot/Program.cs
@@ -0,0 +1,16 @@
+﻿using Lego_api_bot.Features;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lego_api_bot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await BotInitializer.StartWork();
+
+            Thread.Sleep(int.MaxValue);
+        }
+    }
+}
